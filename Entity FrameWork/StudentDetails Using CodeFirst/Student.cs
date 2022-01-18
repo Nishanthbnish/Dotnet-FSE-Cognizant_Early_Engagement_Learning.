e1621236ac7d32f631ba8b2e2273257b94e88d02
Nishanthbnish <bnishanthc@gmail.com>
2022-01-18 09:15:55 +0530

    Create Student.cs

---
+++ Entity FrameWork/StudentDetails Using CodeFirst/Student.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement   //DO NOT Change the namespace name
+{
+    public class Student     //DO NOT Change the class name
+    {
+       public int StudentId{set;get;}
+       public string StudentName{get;set;}
+       public DateTime EnrolledDate{get;set;}
+       public string Department{get;set;}
+       public long PhoneNumber{set;get;}
+       
+    }
+}
