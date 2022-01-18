25bc1460231cdc6c6f443db5c5323ecce631e17c
Nishanthbnish <bnishanthc@gmail.com>
2022-01-18 09:15:25 +0530

    Create Course.cs

---
+++ Entity FrameWork/CourseRepository/Course.cs
@@ -0,0 +1,19 @@
+//THIS CLASS IS FOR REFERENCE. YOU NEED NOT CHANGE
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1   //DO NOT Change the namespace name
+{
+  public  class Course   //DO NOT Change the class name
+    {
+        
+        public int CourseId { get; set; }
+        public String CourseName { get; set; }
+        public double CourseFee { get; set; }
+        public int Duration { get; set; }
+        public String InstructorName { get; set; }
+    }
+}
