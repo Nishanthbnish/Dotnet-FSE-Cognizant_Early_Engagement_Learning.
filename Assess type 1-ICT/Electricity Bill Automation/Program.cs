7dfdf478b2e3a4fdcc481b0d6027bcff8a5d886f
Nishanthbnish <bnishanthc@gmail.com>
2022-01-25 09:48:03 +0530

    Create Program.cs

---
+++ Assess type 1-ICT/Electricity Bill Automation/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Collections;
+using System.Data;
+using System.Configuration;
+
+namespace BillAutomation         //DO NOT change the namespace name
+{
+    public class Program        //DO NOT change the class name
+    {
+        
+        static void Main(string[] args)  //DO NOT change the 'Main' method signature
+        {
+            //Implement the code here
+        }
+    }
+	
+	public class BillValidator{      //DO NOT change the class name
+    
+        public String ValidateUnitsConsumed(int UnitsConsumed)      //DO NOT change the method signature
+        {
+            //Implement code here
+        }
+    }
+}
