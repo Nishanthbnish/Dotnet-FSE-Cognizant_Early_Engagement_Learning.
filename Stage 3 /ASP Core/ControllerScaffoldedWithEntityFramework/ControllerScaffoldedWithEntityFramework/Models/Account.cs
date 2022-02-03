1a4ad4dfc720c76b07060dcbf81b5e9db86883e7
Nishanthbnish <bnishanthc@gmail.com>
2022-02-03 12:44:06 +0530

    Create Account.cs

---
+++ Stage 3 /ASP Core/ControllerScaffoldedWithEntityFramework/ControllerScaffoldedWithEntityFramework/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControllerScaffoldedWithEntityFramework.Models
+{
+    public class Account
+    {
+        [Key]
+        public int AccountId { get; set; }
+
+        public string AccountName { get; set; }
+    }
+}
