6bc64e6736ede7a922b7685ce60ea5e1f45be5c0
Nishanthbnish <bnishanthc@gmail.com>
2022-02-03 12:43:36 +0530

    Create 20210306052423_InitialMigration.cs

---
+++ Stage 3 /ASP Core/ControllerScaffoldedWithEntityFramework/ControllerScaffoldedWithEntityFramework/Migrations/20210306052423_InitialMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ControllerScaffoldedWithEntityFramework.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    AccountId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.AccountId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
