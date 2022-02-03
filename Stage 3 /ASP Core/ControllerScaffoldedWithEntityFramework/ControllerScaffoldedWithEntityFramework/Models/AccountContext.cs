d6700a3b3f7fc6753856007ecf31cf68b6c3a216
Nishanthbnish <bnishanthc@gmail.com>
2022-02-03 12:44:11 +0530

    Create AccountContext.cs

---
+++ Stage 3 /ASP Core/ControllerScaffoldedWithEntityFramework/ControllerScaffoldedWithEntityFramework/Models/AccountContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControllerScaffoldedWithEntityFramework.Models
+{
+    public class AccountContext : DbContext
+    {
+        public AccountContext(DbContextOptions<AccountContext> options)
+            :base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; } 
+    }
+}
